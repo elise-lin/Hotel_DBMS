7bede46cfd3f6737c29a49097947e8725f954deb
elise-lin <32940564+elise-lin@users.noreply.github.com>
2019-12-06 11:49:35 -0800

    Uploading measure file

--- measure.sh
+++ measure.sh
@@ -1,11 +1,23 @@
+# #!/bin/bash
+# psql -h localhost -p $PGPORT $DB_NAME < create_tables.sql > /dev/null
+# sleep 5
+
+# echo "Query time without indexes"
+# cat <(echo '\timing') queries.sql | psql -h localhost -p $PGPORT $DB_NAME | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
+
+# psql -h localhost -p $PGPORT $DB_NAME < create_indexes.sql > /dev/null
+
+# echo "Query time with indexes"
+# cat <(echo '\timing') queries.sql |psql -h localhost -p $PGPORT $DB_NAME | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
+
 #!/bin/bash
-psql -h localhost -p $PGPORT $DB_NAME < create_tables.sql > /dev/null
+psql -h localhost -p $PGPORT $DB_NAME < create.sql > /dev/null
 sleep 5
 
 echo "Query time without indexes"
 cat <(echo '\timing') queries.sql | psql -h localhost -p $PGPORT $DB_NAME | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
 
-psql -h localhost -p $PGPORT $DB_NAME < create_indexes.sql > /dev/null
+psql -h localhost -p $PGPORT $DB_NAME < create_index.sql > /dev/null
 
 echo "Query time with indexes"
 cat <(echo '\timing') queries.sql |psql -h localhost -p $PGPORT $DB_NAME | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
